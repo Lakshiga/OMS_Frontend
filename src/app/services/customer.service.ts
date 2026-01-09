f2a8abc4ab965aa63c5fc92e4521a7cb4bdee795
ladshi <shikaladhu@gmail.com>
2026-01-09 07:05:12 +0530

    corrected naming mismatch

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -11,27 +11,22 @@ export class CustomerService {
 
   constructor(private http: HttpClient) { }
 
-  // FR-05: Get all customers
   getAllCustomers(): Observable<Customer[]> {
     return this.http.get<Customer[]>(this.apiUrl);
   }
 
-  // Get customer by ID
   getCustomerById(id: number): Observable<Customer> {
     return this.http.get<Customer>(`${this.apiUrl}/${id}`);
   }
 
-  // FR-03: Create new customer
   createCustomer(customer: CustomerRequest): Observable<Customer> {
     return this.http.post<Customer>(this.apiUrl, customer);
   }
 
-  // FR-04: Update customer
   updateCustomer(id: number, customer: CustomerRequest): Observable<Customer> {
     return this.http.put<Customer>(`${this.apiUrl}/${id}`, customer);
   }
 
-  // FR-06: Delete customer
   deleteCustomer(id: number): Observable<any> {
     return this.http.delete(`${this.apiUrl}/${id}`);
   }
--- src/app/types/customer.ts
+++ src/app/types/customer.ts
@@ -1,6 +1,6 @@
 export interface Customer {
   id: number;
-  customerCode: string;
+  CustomerCode: string;
   name: string;
   email?: string;
   phone?: string;
@@ -10,7 +10,7 @@ export interface Customer {
 }
 
 export interface CustomerRequest {
-  customerCode: string;
+  CustomerCode: string;
   name: string;
   email?: string;
   phone?: string;
